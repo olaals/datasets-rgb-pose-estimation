5c9396c432f3258e76b8d51c17c9658302f653d2
Ola Alstad <olaalstad96@gmail.com>
2022-05-25 01:20:54 +0200

    Add randomized z rotation of env map

--- blender-cache/script.py
+++ blender-cache/script.py
@@ -2,6 +2,8 @@ import numpy as np
 import pickle
 import os
 import bpy
+import math
+import random
 
 def delete_all_entities():
     bpy.ops.object.select_all(action='SELECT')
@@ -49,34 +51,38 @@ def add_glb(file_path):
     print(f'\n glb file path: {file_path}\n')
     bpy.ops.import_scene.gltf(filepath=file_path)
 
-def add_hdr(hdr_path):
+def add_hdr(hdr_path, z_rot_deg):
     C = bpy.context
     scn = C.scene
 
     # Get the environment node tree of the current scene
     node_tree = scn.world.node_tree
     tree_nodes = node_tree.nodes
-
     # Clear all nodes
     tree_nodes.clear()
 
+    node_map = tree_nodes.new(type='ShaderNodeMapping')
+    node_map.location = -600,0
+    node_map.inputs[2].default_value[2] = z_rot_deg/180*math.pi
+    texture_node = tree_nodes.new(type='ShaderNodeTexCoord')
+    texture_node.location=-900,0
     # Add Background node
     node_background = tree_nodes.new(type='ShaderNodeBackground')
-
+    node_background.location = 0, 100
     # Add Environment Texture node
     node_environment = tree_nodes.new('ShaderNodeTexEnvironment')
     # Load and assign the image to the node property
     node_environment.image = bpy.data.images.load(hdr_path) # Relative path
     node_environment.location = -300,0
-
     # Add Output node
     node_output = tree_nodes.new(type='ShaderNodeOutputWorld')
     node_output.location = 200,0
-
     # Link all nodes
     links = node_tree.links
     link = links.new(node_environment.outputs["Color"], node_background.inputs["Color"])
     link = links.new(node_background.outputs["Background"], node_output.inputs["Surface"])
+    link = links.new(node_map.outputs[0], node_environment.inputs[0])
+    link = links.new(texture_node.outputs[0], node_map.inputs[0])
 
 
 
@@ -106,7 +112,9 @@ for obj in objects:
         assert False
 hdr = scene_config["hdr"]
 hdr_path = hdr["path"]
-add_hdr(hdr_path)
+
+z_rot_deg = random.randint(0,360)*1.0
+add_hdr(hdr_path, z_rot_deg)
 
 render_config = bl_conf["render_config"]
 
