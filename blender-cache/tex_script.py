f03031af57e99653dd0836ccf45bdca10d7f4624
Ola Alstad <olaalstad96@gmail.com>
2022-05-24 12:56:19 +0200

    Add gltf image texturing w blender

--- blender-cache/tex_script.py
+++ blender-cache/tex_script.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 import os
 import bpy
+from bpy import context, data, ops
 
 def delete_all_entities():
     bpy.ops.object.select_all(action='SELECT')
@@ -12,25 +13,63 @@ def add_ply(file_path):
     bpy.ops.import_mesh.ply(filepath = file_path)
 
 
+def texturize_object(mesh_path, blender_conf, save_path, texture_path):
+    bl_exec_path = blender_conf["exec_path"]
+    py_path = blender_conf["py_script_path"]
+    cache_dir = blender_conf["cache_dir"]
+    texturize_conf = {
+            "mesh_path":mesh_path,
+            "save_path":save_path,
+            "texture_path":texture_path,
+    }
+    return texturize_conf
 
-delete_all_entities()
+def texturize(bl_conf):
+    mesh_path = bl_conf["mesh_path"]
+    texture_path = bl_conf["texture_path"]
+    save_path = bl_conf["save_path"]
+    add_ply(mesh_path)
+    smart_project_uv()
+    ob = bpy.context.active_object
+    texture_mat(ob, texture_path)
+    bpy.ops.export_scene.gltf(
+        filepath=save_path,
+        use_selection=True,
+    )
+
+
+def smart_project_uv():
+    bpy.ops.object.editmode_toggle()
+    bpy.ops.uv.smart_project()
+    bpy.ops.object.editmode_toggle()
+
+
+
+def texture_mat(ob, texture_path):
+    mat = bpy.data.materials.new(name="New_Mat")
+    mat.use_nodes = True
+    bsdf = mat.node_tree.nodes["Principled BSDF"]
+    texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
+    texImage.image = bpy.data.images.load(texture_path)
+    mat.node_tree.links.new(bsdf.inputs['Base Color'], texImage.outputs['Color'])
+
+    # Assign it to object
+    if ob.data.materials:
+        ob.data.materials[0] = mat
+    else:
+        ob.data.materials.append(mat)
+
+
+if __name__ == '__main__':
+    bl_conf_pkl = open(os.path.join("blender-cache", "bl_conf_tex.pkl"),'rb')
+    bl_conf = pickle.load(bl_conf_pkl)
+    bl_conf_pkl.close()
+    texturize(bl_conf)
 
 
 
 
 
-bl_conf_pkl = open(os.path.join("blender-cache", "bl_conf_tex.pkl"),'rb')
-bl_conf = pickle.load(bl_conf_pkl)
-bl_conf_pkl.close()
-
-scene_config = bl_conf["scene_config"]
-mesh_path = scene_config["mesh_path"]
-add_ply(mesh_path)
-
-
-
-
-write_path = render_config["write_path"]
 
 
 
--- blender_texturize.py
+++ blender_texturize.py
@@ -1,14 +1,23 @@
 
+import pickle
+import os
+import subprocess
 
 
-def texturize_object(mesh_path, blender_conf, save_path, texture_path, py_path):
+def texturize_object(mesh_path, blender_conf, save_path, texture_path):
+    bl_exec_path = blender_conf["exec_path"]
+    py_path = blender_conf["py_script_path"]
+    cache_dir = blender_conf["cache_dir"]
     texturize_conf = {
             "mesh_path":mesh_path,
+            "save_path":save_path,
+            "texture_path":texture_path,
     }
+    call_blender_subprocess(bl_exec_path, py_path, cache_dir, texturize_conf)
 
 
 
-def call_blender_subprocess(bl_exec_path, py_path, cache_dir, texturize_conf):
+def call_blender_subprocess(bl_exec_path, py_path, cache_dir, texture_conf):
     bl_pickle = open(os.path.join(cache_dir, "bl_conf_tex.pkl"), 'wb')
     pickle.dump(texture_conf, bl_pickle)
     bl_pickle.close()
@@ -21,11 +30,14 @@ def call_blender_subprocess(bl_exec_path, py_path, cache_dir, texturize_conf):
 
 if __name__ == '__main__':
     obj_path = "3d-datasets/ModelNet10-norm-clean-ply/chair/train/chair_0729_opt_HR.ply"
-    save_path = "blender-cache/test.png"
+    save_path = "blender-cache/test.gltf"
+    texture_path = "/home/ola/projects/datasets-rgb-pose-estimation/texture-datasets/dtd-textures/dotted/dotted_0174.jpg"
     blender_conf = {
         "exec_path": "/home/ola/library/blender312/blender",
         "py_script_path": "blender-cache/tex_script.py",
         "cache_dir": "blender-cache",
     }
+    texturize_object(obj_path, blender_conf, save_path, texture_path)
+    
     #call_blender_subprocess(bl_exec_path, py_path, cache_dir, texture_conf):
 
